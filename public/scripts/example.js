bbd6435ad44703ead32da173cd0d66080c85408e
Yutaka OGASAWARA <y.ogagaga@gmail.com>
2016-06-07 09:32:51 +0900

    Add Component to CommentList,CommentForm

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -3,11 +3,36 @@ var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
-        Hello, world! I am a CommentBox.
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
       </div>
     );
   }
 });
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello,world! I am a Commnet List.
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+// ルートコンポーネントのインスタンスを作成するので
+// 最後の行に書く
 ReactDOM.render(
   <CommentBox />,
   document.getElementById('content')
