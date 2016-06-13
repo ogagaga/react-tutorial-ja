0c290e4de258eebdc5831a4e82c9946d3ed78ddf
Yutaka OGASAWARA <y.ogagaga@gmail.com>
2016-06-13 09:45:36 +0900

    Add onChange method

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -89,8 +89,18 @@ var CommentForm = React.createClass({
   render: function() {
     return (
       <form className="commentForm" onSubmit={this.handleSubmit}>
-        <input type="text" placeholder="Your name" ref="author" />
-        <input type="text" placeholder="Say something..." ref="text" />
+        <input
+          type="text"
+          placeholder="Your name"
+          value={this.state.author}
+          onChange={this.handleAuthorChange}
+        />
+        <input
+          type="text"
+          placeholder="Say something..."
+          value={this.state.text}
+          onChange={this.handleTextChange}
+        />
         <input type="submit" value="Post" />
       </form>
     );
