b94eadaccbabed4e618c13ca467a6f1f5976c930
Yutaka OGASAWARA <y.ogagaga@gmail.com>
2016-06-13 09:36:52 +0900

    Add handle method to input tag

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -66,11 +66,31 @@ var Comment = React.createClass({
 });
 
 var CommentForm = React.createClass({
+  getInitialState: function() {
+    return {author: '', test: ''};
+  },
+  handleAuthorChange: function(e) {
+    this.setState({author: e.target.value});
+  },
+  handleTextChange: function(e) {
+    this.setState({text: e.target.value});
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var author = this.state.author.trim();
+    var text = this.state.text.trim();
+    if (!text || !author) {
+      return;
+    }
+    // TODO: サーバにリクエストを送信
+    this.setState({author: '', text: ''});
+    return;
+  },
   render: function() {
     return (
-      <form className="CommentForm">
-        <input type="text" placeholder="Your name" />
-        <input type="text" placeholder="Say something..." />
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Your name" ref="author" />
+        <input type="text" placeholder="Say something..." ref="text" />
         <input type="submit" value="Post" />
       </form>
     );
